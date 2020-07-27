9aed9d3bbc95ea441ff33abd91847b91cb26f723
metacogpe <metacog@kakao.com>
2020-07-27 20:40:10 +0900

    Delete script-test.js

--- module4-solution/script-test.js
+++
@@ -1,6 +0,0 @@
-(function(names) {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (var i = 0; i < names.length; i++) {
-        console.log("Hello" + names[i])
-    }
-})();
