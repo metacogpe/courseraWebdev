7bfecc75337e2715085e7e826b575de0182d2d9b
metacogpe <metacog@kakao.com>
2020-07-27 20:36:30 +0900

    coursera-mod4-HW

---
+++ module4-solution/script-test.js
@@ -0,0 +1,6 @@
+(function(names) {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        console.log("Hello" + names[i])
+    }
+})();
